fa733e6df86c11bbaa35515e0d9ad5e6d4b1378a
valeriahoyos <valehoyos0984@gmail.com>
2023-12-04 15:35:08 -0500

    se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //Entradas del Ploblema
+
+        String nombreUsuario = "Steven Garcia";
+        short edadUsuario = 31;
+        String telefonoUsuario = "3148643384";
+        String documentoIdentidad = "1152443654";
+        int cuotaEnero;
+        int cuotaFebrero;
+        int cuotaMarzo;
+        int cuotaAbril;
+        int cuotaMayo;
+        int cuotaJunio;
+        int cuotaJulio;
+        int cuotaAgosto;
+        int cuotaSeptiembre;
+        int cuotaOctubre;
+        int cuotaNoviembre;
+        String direccionUsuario = "Cr 36A # 75B - 23";
+        double costoInscripcion = 150000.0;
+        float tasaInteresNatillera = 0.1F;
+        double sakdoFinalUsuario;
+
+        //Proceso
+
+        // 1. Asignar un valor especifico a una variable (Recibir Datos)
+
+        Scanner leerDato = new Scanner(System.in);
+
+        System.out.println("Hola soy un IA, cual es tu amor nombre?");
+        leerDato.nextLine();
+
+
+
+
+
+
+
+    }
+}
